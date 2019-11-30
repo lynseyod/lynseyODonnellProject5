6787af3b7f3343f8045c88cd41a041c4453f6fb8
lynseyod <lyns.odonnell@gmail.com>
2019-11-29 21:27:13 -0500

    less ugly buttons :information_desk_person::nail_care::eyes:

--- src/App.js
+++ src/App.js
@@ -87,16 +87,9 @@ class App extends Component {
     })
     const testAThing = arrayToSort.sort();
     const sortedArrayQuestionMark = [];
-// let's fucking pseudo code this shit ok
-//  for each item in testAThing:
-//    find it in fuckingFakeArray.contactobj[toSortBy]
-//    if contactID of thing is NOT in sortedArrayQuestionMark:
-//      push to sortedArrayQuestionMark
 
-// MOTHERFUCKING NESTED FOR LOOPS
-
-  testAThing.forEach((sortedThing) => {
-    let shouldIAddIt = true
+    testAThing.forEach((sortedThing) => {
+      let shouldIAddIt = true
       // test it against each item in fuckingFakeArray
       fuckingFakeArray.forEach((fuckingFakeContact) => {
         if(fuckingFakeContact.contactObj[toSortBy] === sortedThing) {
@@ -126,10 +119,10 @@ class App extends Component {
       <div className="wrapper">
         <Header />
         <main>
-          <h3>Sort by:</h3>
-          <button id="lastContacted" onClick={this.clickSort}>Last Contacted</button>
-          <button id="lastName" onClick={this.clickSort}>Last Name</button>
-          <button id="company" onClick={this.clickSort}>Company</button>
+          <label className="sortLabel">Sort by:</label>
+          <button className="sortButton" id="lastContacted" onClick={this.clickSort}>Last Contacted</button>
+          <button className="sortButton" id="lastName" onClick={this.clickSort}>Last Name</button>
+          <button className="sortButton" id="company" onClick={this.clickSort}>Company</button>
           <button className="newAddy" onClick={this.handleClick}>
             {this.state.displayForm ? <i className="fas fa-times-circle"></i> : <i className="fas fa-plus-circle"></i>}
           </button>
